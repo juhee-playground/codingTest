b4ffa2f3dfaf83b88083f86d9b998a2c76046be7
Juhee <juhee.baek@fitogether.com>
2021-04-23 09:48:57 +0900

    [4/23][코테] 시저암호

--- programmers/caesarCipher.js
+++ programmers/caesarCipher.js
@@ -1,20 +1,34 @@
 function solution(s, n) {
-  var answer = "";
-  let ascii = "";
-  for(let i = 0; i < s.length; i++) {
-      // 문자가 공백이면 패스
-      if(s[i] === " ") {
-          answer += " ";
-      } else {
-          // 문자가 z, Z이면 -26 해주기
-          ascii = s.charCodeAt(i);
-          if(ascii === 90 || ascii === 122) {
-              ascii = ascii - 26;
-          }
-          console.log(ascii, n, " = ", ascii + n);
-          ascii = ascii + n;
-          answer += String.fromCharCode(ascii);
-      }
-  }
-  return answer;
+    var answer = "";
+    const lowerList = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    const upperList = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    for(let i = 0; i < s.length; i++) {
+        // 문자가 공백이면 패스
+        if(s[i] === " ") {
+            answer += s[i];
+            continue;
+        } 
+        // 문자가 대문자인지 소문자인지 판별
+        let isUpperCase = checkUpperCase(s[i]);
+        if(isUpperCase) {
+            answer += changeCipher(upperList, s[i], n);
+        }else {
+            answer += changeCipher(lowerList, s[i], n);
+        }
+    }
+    return answer;
+}
+
+function checkUpperCase(char) {
+    if(char.toUpperCase() === char) {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+function changeCipher(arr, char, n) {
+    let findIndex = arr.indexOf(char);
+    findIndex = (findIndex + n) % 26;
+    return arr[findIndex];
 }
