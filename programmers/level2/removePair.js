7f577185e8cb08f5ed7dd0113481ecc9d465e353
Juhee <juhee.baek@fitogether.com>
2021-05-04 09:45:12 +0900

    [5/3] 코테 효율성 통과시키기

--- programmers/level2/removePair.js
+++ programmers/level2/removePair.js
@@ -1,28 +1,26 @@
 function solution(s)
 {
     var answer = 0;
-    let initChar = s.charAt(0);
-    for(let i = 0; i < s.length; i++) {
-        if(i < s.length-1) {
-            if(s.charAt(i) === s.charAt(i+1)) {
-                let deleteString = s.charAt(i) + s.charAt(i+1);
-                s = s.replace(deleteString, "");
-                i = i > 0 ? i - 2: i - 1;
-            }else {
-                initChar = s.charAt(i);
-            }
-        }else {
-            return answer;
+    let charArr = s.split("");
+    let compareArr = [];
+    for(let i = 0; i < charArr.length; i++) {
+        if(compareArr.length === 0) {
+            compareArr.push( charArr[i]);
+            continue;
         }
 
-        if(s.length === 1 || s.length % 2 === 1) {
-            return answer;
+        let lastCharIndex = compareArr.length-1;
+        let compareStringLastChar = compareArr[lastCharIndex];
+        let charOne = charArr[i];
+        if(compareStringLastChar === charOne) {
+            // 마지막 문자랑 비교한 값 같으면 지우기
+            compareArr.pop();
+        }else {
+            compareArr.push(charOne);
         }
     }
-    
-    if(s.length === 0) {
+    if(compareArr.length === 0) {
         answer = 1;
     }
-
     return answer;
 }
