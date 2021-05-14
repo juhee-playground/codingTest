174f080b786cfa0eb845f180e42146da315307ac
Juhee <juhee.baek@fitogether.com>
2021-05-14 13:04:42 +0900

    [5/14] [코테] last 다리를 지나는 트럭

--- programmers/level2/stackAndQueue/d3_truckPssingTheBridge.js
+++ programmers/level2/stackAndQueue/d3_truckPssingTheBridge.js
@@ -1,37 +1,48 @@
-function solution(bridge_length, weight, truck_weights) {
-  var answer = 0;
+function solution(bridge_length, bridge_weight, waitingTruck) {
   let time = 0;
-  let countTruck = truck_weights.length;
+  let countTruck = waitingTruck.length;
   let passingTruck = [];
   let crossingTruck = [];
-  let waitingTruck = truck_weights;
+  let weightOnBridge = 0;
   // 대기하고 있는 트럭을 조건이 맞을때 마다 한대씩 보낸다.
   while(passingTruck.length < countTruck) {
-      let sumCrossingWeight = sumCrossingTrueckWeight(crossingTruck);
-      for(let i = 1; i <= bridge_length; i++) {
-          // 다리를 빠져나갈 수 있는 조건
-          // 트럭이 지나간 길이가 다리의 길이보다 같을 때
-          if(i === bridge_length) {
-              passingTruck.push(crossingTruck[0]);
-              crossingTruck.shift();
-              console.log("index", answer, passingTruck,crossingTruck, );
-          }
-          
-          // 다리에 오를 수 있는 조건
-          // crossTheTruck +  대기하고 있는 첫번째 트럭의 무게 < 다리가 견딜 수 있는 무게
-          if(sumCrossingWeight + waitingTruck[0] < weight) {
-              crossingTruck.push(waitingTruck[0]);
-              waitingTruck.shift();
-          }
-          answer++;
+    // 시간이 1초 지남
+    time += 1;
+
+    let somethingPass = false;
+
+    // 다리위의 모든 트럭
+    crossingTruck.forEach(truckOnBridge => {
+      const same = truckOnBridge.passingDistance == bridge_length;
+      // 같으면 트럭 나감
+      if (same){
+        somethingPass = true;
+        passingTruck.push(truckOnBridge);
+        weightOnBridge -= truckOnBridge.weight;
+      }else{
+        // 다르면 1 이동
+        truckOnBridge.passingDistance += 1;
       }
-      
+    });
 
+    if (somethingPass){
+      crossingTruck.shift();
+    }
+
+    // 대기 -> 다리 
+    if (waitingTruck.length != 0) {
+      const waitingTruckWeight = waitingTruck[0];
+      // 1. 다리위의 차 무게 + 대기 하고 있는 차의 무게 <= 다리무게 비교
+      const canAddTruck = weightOnBridge + waitingTruckWeight <= bridge_weight;
+      if (canAddTruck){
+
+        // -맞으면 다리위로
+        weightOnBridge += waitingTruckWeight;
+        waitingTruck.shift();
+        crossingTruck.push({weight:waitingTruckWeight, passingDistance:1});
+      }
+    }
   }
-  return answer;
-}
 
-function sumCrossingTrueckWeight(crossingTruck) {
-  const sumWeight = (accumulator, currentValue) => accumulator + currentValue;
-  return crossingTruck.length > 0 ? crossingTruck.reduce(sumWeight) : 0;
+  return time;
 }
