aac9d88e18bb09326b194dbe8804c1b11a1db25b
Juhee <juhee.baek@fitogether.com>
2021-06-08 13:33:35 +0900

    [6/8][코테] N개의 최소공배수 성공한 것

--- programmers/level2/NofNumbersLCM.js
+++ programmers/level2/NofNumbersLCM.js
@@ -1,21 +1,12 @@
 // #programmers, #level2, #LCM, #GCD, #N개의최소공배수
 
 function solution(arr) {
-  // arr.sort((a, b) => b - a);
-  let getGCD = (a, b) => (b > 0 ? getGCD(b, a % b) : a);
-  let getLcm = (a, b, gcd) => (a * b) / gcd;
-  
-  let initValue = arr[0];
-  const gcd = arr.reduce((initValue, value, index) => {
-      console.log(initValue, value);
-      return initValue % value === 0 ? parseInt(initValue / value) : getGCD(initValue, value);
-  });
-  console.log("gcd", gcd);
+  let getGCD = (a, b) => ((a % b) === 0 ? b : getGCD(b, a % b));
+  let getLcm = (a, b) => (a * b) / getGCD(a, b);
 
-  const lcm = arr.reduce((initValue, value) => {
-      return getLcm(initValue, value, gcd); 
-  });
-  // console.log("lcm", lcm);
-  
+  let lcm = 1;
+  for(let i = 0; i < arr.length; i++) {
+    lcm = getLcm(lcm, arr[i]);
+  }
   return lcm;
 }
