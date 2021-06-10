497029204deb1fcbcf173fc9db3eb53b7aa37557
Juhee <juhee.baek@fitogether.com>
2021-06-10 10:14:04 +0900

    [6/10][아침코테] 실패율

---
+++ programmers/kakao/failureRate.js
@@ -0,0 +1,26 @@
+function solution(N, stages) {
+  var answer = [];
+  let floorFailure = [];
+  for(let f = 1; f <= N; f++) {
+    let notClearPerson = countNotClear(f, stages);
+    let clearStagePerson = countClearStage(f, stages);
+    let failureRate = clearStagePerson / notClearPerson * 100;
+      
+    floorFailure.push({ f, failureRate });
+  }
+  floorFailure.sort(sortByFailureRate);
+  answer = [...new Set(floorFailure.map(item => item.f))];
+  return answer;
+}
+
+function countNotClear(floor, stages) {
+  return stages.filter(stage => floor <= stage).length;
+}
+
+function countClearStage(floor, stages) {
+  return stages.filter(stage => floor === stage).length;
+}
+
+function sortByFailureRate(a, b) {
+  return a.failureRate < b.failureRate ? 1 : a.failureRate > b.failureRate ? -1 : 0;
+}
