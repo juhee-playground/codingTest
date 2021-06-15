7f0774cd670f65356ae57dba79527ebff69fb123
Juhee <juhee.baek@fitogether.com>
2021-06-15 09:43:26 +0900

    [6/15][아침코테] 다트게임
     - 이틀만에 풀다니ㅜㅜ

--- programmers/kakao/dartGame.js
+++ programmers/kakao/dartGame.js
@@ -1,5 +1,15 @@
+  /*
+      Single(S) - 1제곱, Double(D) - 2제곱, Triple(T) - 3제곱 (점수1 , 점수2 , 점수3 )으로 계산된다.
+      스타상(*) 당첨 시 해당 점수와 바로 전에 얻은 점수를 각 2배로 만든다.
+      아차상(#) 당첨 시 해당 점수는 마이너스된다.
+      스타상(*)은 첫 번째 기회에서도 나올 수 있다. 이 경우 첫 번째 스타상(*)의 점수만 2배가 된다.
+      스타상(*)의 효과는 다른 스타상(*)의 효과와 중첩될 수 있다. 이 경우 중첩된 스타상(*) 점수는 4배가 된다.
+      스타상(*)의 효과는 아차상(#)의 효과와 중첩될 수 있다. 이 경우 중첩된 아차상(#)의 점수는 -2배가 된다.
+      Single(S), Double(D), Triple(T)은 점수마다 하나씩 존재한다.
+      스타상(*), 아차상(#)은 점수마다 둘 중 하나만 존재할 수 있으며, 존재하지 않을 수도 있다.
+  */
 function solution(dartResult) {
-  var answer = 0;
+  let answer = 0;
   let dartScoreBoard = {
     1: { score: 0, bonus: "", options: "", sum: 0 },
     2: { score: 0, bonus: "", options: "", sum: 0 },
@@ -33,31 +43,22 @@ function solution(dartResult) {
         }
       }
     }else if(optionsEnum.includes(char)) {
-         // 문자가 옵션을 확인할 수 있는 건지?
+          // 문자가 옵션을 확인할 수 있는 건지?
       dartScoreBoard[count].options = char;
       count++;
     }
   }
-  // console.log("board", dartScoreBoard);
-  /*
-      Single(S) - 1제곱, Double(D) - 2제곱, Triple(T) - 3제곱 (점수1 , 점수2 , 점수3 )으로 계산된다.
-      스타상(*) 당첨 시 해당 점수와 바로 전에 얻은 점수를 각 2배로 만든다.
-      아차상(#) 당첨 시 해당 점수는 마이너스된다.
-      스타상(*)은 첫 번째 기회에서도 나올 수 있다. 이 경우 첫 번째 스타상(*)의 점수만 2배가 된다.
-      스타상(*)의 효과는 다른 스타상(*)의 효과와 중첩될 수 있다. 이 경우 중첩된 스타상(*) 점수는 4배가 된다.
-      스타상(*)의 효과는 아차상(#)의 효과와 중첩될 수 있다. 이 경우 중첩된 아차상(#)의 점수는 -2배가 된다.
-      Single(S), Double(D), Triple(T)은 점수마다 하나씩 존재한다.
-      스타상(*), 아차상(#)은 점수마다 둘 중 하나만 존재할 수 있으며, 존재하지 않을 수도 있다.
-  */
       let totalScore = 0;
-      Object.keys(dartScoreBoard).forEach((key, index) => {
-        let element = dartScoreBoard[key];
-        let score = MathPow(element.score, element.bouns);
+      let orders = Object.keys(dartScoreBoard);
+      orders.forEach((order, index) => {
+        let element = dartScoreBoard[order];
+        let score = MathPow(element.score, element.bonus);
         if(element.options === "#") {
-          element.sum = - score;
+          element.sum = score * -1;
         }else if(element.options === "*") {
+          let key = orders[index-1];
           if(index > 0) {
-            dartScoreBoard[index-1].sum = dartScoreBoard[index-1].score * 2;
+            dartScoreBoard[key].sum = dartScoreBoard[key].sum * 2;
           }
           element.sum = score * 2;
         }else {
@@ -65,21 +66,21 @@ function solution(dartResult) {
         }
       });
   
-      console.log(dartScoreBoard);
+      // console.log(dartScoreBoard);
+    
+    orders.forEach(order => {
+        answer += dartScoreBoard[order].sum;
+    });
+
   return answer;
 }
 
 function MathPow(num, bonus) {
-  switch(bonus) {
-    case "D":
-      pow = 2;
-      break;
-    case "T":
-      pow = 3;
-      break;
-    default: 
-      pow = 1;
-      break;
-  }
-  return Math.pow(num, pow);
+    let pow = 1;
+    if(bonus === "D") {
+        pow = 2;
+    }else if(bonus === "T") {
+        pow = 3;
+    }
+    return Math.pow(num, pow);
 }
