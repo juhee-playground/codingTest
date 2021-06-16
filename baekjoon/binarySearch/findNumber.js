f781d1cd395ee8ae4c6c4f15554be0598ecba529
Juhee <juhee.baek@fitogether.com>
2021-06-16 20:27:06 +0900

    [알고리즘 뿌시기][백준] 이분탐색 1단계 성공
    - 이렇게 푸는게 맞는지 모르겠네

--- baekjoon/binarySearch/findNumber.js
+++ baekjoon/binarySearch/findNumber.js
@@ -1,3 +1,5 @@
+//FIXME: 작업해야함. 이거는 대충 쓴거임.
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -20,23 +22,37 @@ rl.on('line', function(line) {
   if(count === 4) {
     rl.close();
   }
-}).on("close", function findNumber() {
+}).on("close", function factorial() {
   // 함수 구하기
-  console.log(input);
   numberArrLen = input[0][0];
   numberArr = input[1].slice(0, Number(numberArrLen));
   compareArrLen = input[2][0];
   compareArr = input[3].slice(0, Number(compareArrLen));
+  // console.log(numberArrLen, numberArr, compareArrLen, compareArr);
+  // 먼저 정렬해주기
+  numberArr.sort((a, b) => a - b);
 
   for(let i = 0; i < compareArr.length; i++) {
-    // 구하고자 하는 값 = compareArr[i]
-    if(numberArr.includes(compareArr[i])) {
-      output = 1;
-      console.log(output);
-    }else {
-      output = 0;
-      console.log(output);
+    let left = 0;
+    let right = numberArr.length - 1;
+    // console.log("찾을값", compareArr[i]);
+    while(left <= right) {
+      output[i] = 0;
+      let mid = parseInt((left + right) / 2);
+      // 구하고자 하는 값 = compareArr[i]
+      // console.log("compare", numberArr[mid], compareArr[i]);
+      if(numberArr[mid] > compareArr[i]) {
+        right = mid - 1;
+      }else if(numberArr[mid] < compareArr[i]){
+        left = mid + 1;
+      }else {
+        // console.log("findIt");
+        output[i] = 1;
+        break;
+      }
     }
+    console.log(output[i]);
   }
+  
   process.exit();
 });
