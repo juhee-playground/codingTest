abe1bfcad2b4616f37199d3bf59b5935397df9d8
Juhee <juhee.baek@fitogether.com>
2021-06-23 14:21:06 +0900

    [백준] 코드정리

--- baekjoon/binarySearch/cardNumber.js
+++ baekjoon/binarySearch/cardNumber.js
@@ -29,35 +29,48 @@ rl.on('line', function(line) {
   compareArr = input[3].slice(0, Number(compareArrLen));
   // 먼저 정렬해주기
   haveCardArr.sort((a, b) => a - b);
+  // console.log("arr",haveCardArr);
 
   for(let i = 0; i < compareArr.length; i++) {
-    
     let search = [];
     let startIndex = 0;
-    let endIndex = haveCardLen - 1;
-    // console.log("찾을값", compareArr[i]);
+    let endIndex = haveCardLen;
     while(startIndex <= endIndex) {
       output[i] = 0;
       let midIndex = parseInt((startIndex + endIndex) / 2);
-      // 구하고자 하는 값 = compareArr[i]
-      console.log("index", startIndex, midIndex, endIndex);
-      console.log("compare", haveCardArr[midIndex], compareArr[i]);
       if(haveCardArr[midIndex] > compareArr[i]) {
         endIndex = midIndex - 1;
-        search = haveCardArr.slice(startIndex, endIndex);
+        search = haveCardArr.slice(startIndex, endIndex+1);
       }else if(haveCardArr[midIndex] < compareArr[i]){
         startIndex = midIndex + 1;
-        search = haveCardArr.slice(startIndex, endIndex);
+        search = haveCardArr.slice(startIndex, endIndex+1);
       }else {
-        if(m)
-        search = haveCardArr.slice(startIndex, endIndex);
-        console.log("findIt", search);
-        output[i] = search.length;
+        endIndex = midIndex - 1;
+        startIndex = midIndex + 1;
+        let lowerBound = endIndex;
+        let upperBound = startIndex;
+        
+        let count = upperBound - lowerBound + 1;
+        
+        // console.log("before search", search, compareArr[i]);
+        // if(haveCardArr[startIndex] === haveCardArr[midIndex-1] && haveCardArr[midIndex-1] === haveCardArr[endIndex-1] ) {
+        //   console.log("그대로");
+        //   break;
+        // }else if(haveCardArr[startIndex] < compareArr[i]){
+        //   let index = search.indexOf(compareArr[i])
+        //   search = search.slice(index, search.length);
+        // }else {
+        //   search = search.reverse();
+        //   let index = search.indexOf(compareArr[i])
+        //   search = search.slice(index, search.length);
+        // }
+        // search = search.filter(card => card === compareArr[i]);
+        output.push(count);
         break;
       }
     }
-    console.log(output.join(" "));
   }
+  console.log(output.join(" "));
   
   process.exit();
 });
