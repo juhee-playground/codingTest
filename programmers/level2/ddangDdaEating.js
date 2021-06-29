ed2fd127c37d50a5250ade9d3e176ebcd49f6148
Juhee <juhee.baek@fitogether.com>
2021-06-29 18:11:31 +0900

    [6/29][아침코테] 땅따먹기 dp문제
    dp알고리즘 활용하여 풀었다 12점ㅋㅋㅋㅋㅋㅋ

--- programmers/level2/ddangDdaEating.js
+++ programmers/level2/ddangDdaEating.js
@@ -1,28 +1,33 @@
+// #아침코테 #프로그래머스 #level2 #땅따먹기
 function solution(land) {
-  var answer = 0;
-  let preIndex = 0;
-  let curIndex = 0;
-  let maxNumber = 0;
-  let maxIndex = 0;
-  for(let i = 0; i < land.length; i++) {
-      let arr = land[i];
-      maxNumber = Math.max(...arr);
-      maxIndex = arr.indexOf(maxNumber);
-      // 다음 Arr에서 맥스값과 저장한 인덱스값 비교
-      console.log(preIndex, maxIndex);
-      if(i > 0 && preIndex === maxIndex) {
-          // 같으면 삭제하고 다음꺼에서 맥스값 찾고 더하기, 맥스의 인덱스값 저장.
-          // 삭제 하니까 인덱스가 밀리는 현상 발생 0으로 변경해주기 
-          arr.splice(maxIndex, 1, 0);
-          maxNumber = Math.max(...arr);
-          maxIndex = arr.indexOf(maxNumber);
-      }
+    var answer = 0;
+    let initArr = [];
+    // 초기값 세팅
+    for(let i = 0; i < land[0].length; i++) {
+        initArr[i] = land[0][i];
+    }
 
-      console.log(arr, maxNumber, maxIndex)
-      // 현재 Arr에서 맥스값 찾고 더하기, 맥스의 인덱스값 저장.
-      // 다르면 맥스값 더하고 맥스의 인덱스값 저장
-      preIndex = maxIndex;
-      answer += maxNumber;
-  }
-  return answer;
+    for(let i = 1; i < land.length; i++) {
+        let curArr = land[i];
+        let maxArr = whatIsmaxSum(initArr, curArr);
+
+        initArr = maxArr;
+    }
+    answer = Math.max(...initArr);
+    return answer;
+}
+
+function whatIsmaxSum(initArr, curA) {
+    let max = 0;
+    let arr = [];
+    for(let i = 0; i < curA.length; i++) {
+        max = 0;
+        for(let j = 0; j < curA.length; j++) {
+            if(j !== i && max < curA[i] + initArr[j]) {
+                max = curA[i] + initArr[j];
+            }
+        }
+        arr.push(max);
+    }
+    return arr;
 }
