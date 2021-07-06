cfae81fc01bd3d1c86062de57275bd1f3618ffb1
Juhee <juhee.baek@fitogether.com>
2021-07-06 21:09:48 +0900

    [7/6][저녁코테] 스택 프린터

---
+++ programmers/level2/stackAndQueue/printer.js
@@ -0,0 +1,35 @@
+function solution(priorities, location) {
+  let printerStack = [];
+  console.log(location);
+  let findFirstIndex = location;
+  // 다 빠져나갔을때 끝
+  while(priorities.length > 0) {
+      // index = 0, index 배열의 길이 - 1
+
+      let printItem = priorities[0];
+      priorities.shift();
+
+      let maxNumber = Math.max(...priorities);
+      if(printItem < maxNumber) {
+          // 지금 있는 걸 맨 뒤로 보내기
+          priorities.push(printItem);
+      }
+      else {
+           // 맨 앞에 추가 // 빠지기
+          printerStack.push(printItem);
+      }
+      
+      // 맨 앞을 구하는 
+      if(findFirstIndex !== 0) {
+          // index = 0 이 아닐 때 index - 1
+          findFirstIndex--;
+      }else if(findFirstIndex === 0 && printItem < maxNumber) {
+          // index = 0 이고 나미자 값중에 클 때 index = 배열의 길이 - 1
+          findFirstIndex = priorities.length - 1;
+      }else if(findFirstIndex === 0 && printItem >= maxNumber) {
+          // index = 0 이고 나미자 값들이 다 작을 때 끝
+          console.log("end", printItem,  printerStack.length);
+          return printerStack.length;
+      }
+  }
+}
