d1957dbdd7f55c2fa4a4fd2d33351352d27de45f
Juhee <juhee.baek@fitogether.com>
2021-07-08 14:11:50 +0900

    [아침코테] 구명보트 효율성극대화 시킴

--- programmers/level2/greedy/saveBoat.js
+++ programmers/level2/greedy/saveBoat.js
@@ -2,25 +2,25 @@
 function solution(people, limit) {
     people.sort((a,b) => a-b);
     let answer = 0;
-    
+    let minIndex = 0
+    let maxIndex = people.length -1
     // 남아있는 사람이 없을 때
-    while (people.length > 0){
+    while (minIndex <= maxIndex){
         // 한 사람만 남아 있을 때
-        if (people.length == 1){ 
+        if (minIndex == maxIndex){ 
             answer++;
             break;
         }
-        let minP = people[0];
-        let maxP = people[people.length-1];
+        let minP = people[minIndex];
+        let maxP = people[maxIndex];
         let sum = minP + maxP;
-        
         if (sum <= limit){
             // minP maxP 나가
-            people.pop()
-            people.shift()
+            minIndex++;
+            maxIndex--;
         }else{
             // maxP 나가
-            people.pop()
+            maxIndex--;
         }
         answer++;
     }
