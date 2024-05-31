15b0e622d8f09b857de3d2e7cd53fd5d7c273050
JUHEEBAEK <bjh9287@naver.com>
2024-05-31 19:17:21 +0900

    Style: 템플릿들 폴더 안으로 넣기

--- 03_NInputSplitLine.js
+++
@@ -1,3 +0,0 @@
-// 3. 여러 줄의 값들을 입력받을 때
-const filePath = process.platform === 'linux' ? 'dev/stdin' : '../../input.txt';
-const input = fs.readFileSync(filePath).toString().trim().split("\n");
--- 04_NInputSplitBlank.js
+++
@@ -1,3 +0,0 @@
-// 4. 첫 번째 줄에 자연수 n을 입력받고, 그 다음줄에 공백으로 구분된 n개의 값들을 입력받을 때
-const filePath = process.platform === 'linux' ? 'dev/stdin' : '../../input.txt';
-const [n, ...arr] = fs.readFileSync(filePath).toString().trim().split(/\s/);
